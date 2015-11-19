78a832623db7391c297aa8d32a5dda03c7dea962
Tarrell Rodrigues <tarrellrodrigues@gmail.com>
2015-11-19 21:06:25 +0000

    added routs

--- app/app.js
+++ app/app.js
@@ -2,14 +2,39 @@ angular
     .module('africalooks', [
         'ngStamplay',
         'ui.router',
-        'app.routes'
+        'app.routes',
+        'app.admin',
+        'app.authenticate',
+        'app.checkout',
+        'app.home',
+        'app.product',
+        'app.profile',
+        'UserService'
+       // 'app.shop'
     ])
-    .controller('MainController', MainController);
+    .controller('MainController',['User', '$rootScope', MainController]);
 
 /**
  * The main controller for our application
  */
-function MainController() {
+function MainController(User, $rootScope) {
     var main = this;
+     $rootScope.currentUser = {}; // creating this object to hold our current users info
 
+  // get the current user and bind their data to $rootScope.currentUser object
+  User.getCurrent()
+    .then(function(data) {
+      if (data.get('_id')) {
+        $rootScope.currentUser.id    = data.get('_id');
+        $rootScope.currentUser.name  = data.get('displayName');
+        $rootScope.currentUser.image = data.get('profileImg');
+      } else {
+        // clear the current user just to be sure
+        $rootScope.currentUser = {};
+      }
+    });
+    function logout() {
+    User.logout();
+    $rootScope.currentUser = {};
+  }  
 }    
